c057db6d79beabb0601b586b5360245a80b58e91
Lauren Peabody <lauren.peabody@nhrec.org>
2025-11-05 18:24:14 -0800

    Finished workshop 3

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,5 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 // ACTIVITY: Create routes that call controller functions when rrequested
+router.get('/polls', pollController.getPolls);
+router.get('/polls/id/:id', pollController.getPoll);
+
+router.post('/polls', postController.postPolls);
+router.post('/vote', postController.postVote);
 
 module.exports = router;
